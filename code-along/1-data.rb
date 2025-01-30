fe28bb2edd3a0308f736d10894501e48bc7fea40
kelsey Brown <kelsey.brown@kellogg.northwestern.edu>
2025-01-30 01:03:42 +0000

    practice ruby code along

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -1,19 +1,63 @@
-# To run this code, be sure your current working directory
-# is the same as where this file is located and then run:
-# ruby 1-data.rb
+# # To run this code, be sure your current working directory
+# # is the same as where this file is located and then run:
+# # ruby 1-data.rb
 
-# In Ruby, there are different *types* of data:
+# # In Ruby, there are different *types* of data:
 
-# Numbers
+# # Numbers
 
-# Non-numbers
+# puts 2
+# puts 5
 
-# True, False
+# puts 5 + 2 
+# puts 5 - 2
+# puts 5 * 2
+# puts 5 / 2.0
 
-# Nothing
+# # Non-numbers
 
-# Variables
+# puts "Hello, world!"
 
-# Combine strings and variables
+# puts "Tacos are" + " delish!"
+# puts "tacos" * 3 
+# puts "tacos:" + 3.to_s
+
+# # True, False
+# puts true
+# puts false 
+
+# # Nothing
+
+# puts nil
+
+# # Variables
+
+# x = 10 
+# y = 3
+# puts x * y 
+
+# food = "tacos"
+# quantity = 3
+
+# puts food * quantity 
+
+# # Combine strings and variables
+
+# first_name = "Ian"
+# greeting = "Hello, #{first_name}"
+
+# puts greeting
+
+# puts "tacos: #{quantity}"
+# puts "#{food}: #{quantity}"
 
 # String manipulation
+
+puts "Hello".length
+puts "how are you today?".capitalize
+puts "computer".reverse
+
+creed = "This IS The Way"
+puts creed.upcase
+puts creed.downcase
+puts creed.swapcase
