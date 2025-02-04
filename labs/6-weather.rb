321901274735db926b7dee4d05b42efbc63bbb2c
kelsey Brown <kelsey.brown@kellogg.northwestern.edu>
2025-02-04 19:32:30 +0000

    lab 6 practice

--- labs/6-weather.rb
+++ labs/6-weather.rb
@@ -22,13 +22,13 @@ weather_data = {
     conditions: "Sunny"
   },
   forecast: [
-    "Day 1" => { temperature: 65, conditions: "Mostly Cloudy", precipitation: 0.4 },
-    "Day 2" => { temperature: 70, conditions: "Partly Cloudy", precipitation: 0.2 },
-    "Day 3" => { temperature: 62, conditions: "Sunny", precipitation: 0.1 },
-    "Day 4" => { temperature: 68, conditions: "Sunny", precipitation: 0.1 },
-    "Day 5" => { temperature: 65, conditions: "Partly Cloudy", precipitation: 0.7 },
-    "Day 6" => { temperature: 72, conditions: "Partly Cloudy", precipitation: 0.4 },
-    "Day 7" => { temperature: 60, conditions: "Rainy", precipitation: 0.9 }
+     { temperature: 65, conditions: "Mostly Cloudy", precipitation: 0.4 },
+    { temperature: 70, conditions: "Partly Cloudy", precipitation: 0.2 },
+     { temperature: 62, conditions: "Sunny", precipitation: 0.1 },
+    { temperature: 68, conditions: "Sunny", precipitation: 0.1 },
+    { temperature: 65, conditions: "Partly Cloudy", precipitation: 0.7 },
+    { temperature: 72, conditions: "Partly Cloudy", precipitation: 0.4 },
+    { temperature: 60, conditions: "Rainy", precipitation: 0.9 }
   ]
 }
 
@@ -37,19 +37,60 @@ puts "Currently it is #{weather_data[:current][:temperature]} degrees and #{weat
 
 # Print forecast
 puts "Forecast for the next 7 days:"
-# weather_data[:forecast].each do |day, details|
-#   temperature = details[:temperature]
-#   conditions = details[:conditions]
-#   precipitation = details[:precipitation]
-  
-#   forecast_str = " - #{temperature} degrees and #{conditions}"
+# puts "Day 1 #{weather_data[:forecast][0][:temperature]} degrees and #{weather_data[:forecast][0][:conditions]} chance of rain #{weather_data[:forecast][0][:precipitation]}."
+# puts "Day 2 #{weather_data[:forecast][1][:temperature]} degrees and #{weather_data[:forecast][1][:conditions]} chance of rain #{weather_data[:forecast][1][:precipitation]}."
+# puts "Day 3 #{weather_data[:forecast][2][:temperature]} degrees and #{weather_data[:forecast][2][:conditions]} chance of rain #{weather_data[:forecast][2][:precipitation]}."
+# puts "Day 4 #{weather_data[:forecast][3][:temperature]} degrees and #{weather_data[:forecast][3][:conditions]} chance of rain #{weather_data[:forecast][3][:precipitation]}."
+# puts "Day 5 #{weather_data[:forecast][4][:temperature]} degrees and #{weather_data[:forecast][4][:conditions]} chance of rain #{weather_data[:forecast][4][:precipitation]}."
+# puts "Day 6 #{weather_data[:forecast][5][:temperature]} degrees and #{weather_data[:forecast][5][:conditions]} chance of rain #{weather_data[:forecast][5][:precipitation]}."
+# puts "Day 7 #{weather_data[:forecast][6][:temperature]} degrees and #{weather_data[:forecast][6][:conditions]} chance of rain #{weather_data[:forecast][6][:precipitation]}."
 
-#   if conditions != "Rainy" && precipitation > 0.5
-#     forecast_str += " with a #{(precipitation * 100).to_i}% chance of rain"
-#   end
+index = 0 
+loop do 
+  weather_output = []
+for temperature in weather_data[:forecast]
+  for conditions in weather_data[:forecast]
+    for precipitation in weather_data[:forecast]
+     puts weather = "#{temperature} degrees and #{conditions} chance of rain #{precipitation}."
+  # temperature = "#{weather_data[:forecast][:temperature]}"
+  # conditions = "#{weather_data[:forecast][:conditions]}" 
+  # precipitation = "#{weather_data[:forecast][:precipitation]}"
+      weather_output.push(weather)
+      index = index + 1
+        if index == 2 
+        break 
+      end 
+    end
+    end 
+  end 
+end 
 
-#   puts forecast_str
+
+
+# weather = "#{weather_data[:forecast][:temperature]} degrees and #{weather_data[:forecast][:conditions]} chance of rain #{weather_data[:forecast][:precipitation]}."
+# puts weather 
+
+if weather == "Rainy" || weather < 0.5
+  puts ""
+else
+  puts "#{weather_data[:forecast][:conditions]}" && "#{weather_data[:forecast][0,6][:precipitation]}"
+end
+# for day in weather_data[:forecast]
+#   # temperature = "#{weather_data[:forecast][:temperature]}"
+#   # conditions = "#{weather_data[:conditions]}"
+#   # precipitation = "#{weather_data[:precipitation]}"
+#   puts "#{weather_data[:forecast]["Day 1"][:temperature]} degrees and #{weather_data[:forecast][:conditions]} chance of rain #{weather_data[:forecast][:precipitation]}"
 # end
+
+
+# weather_shown = weather[0,7]
+
+# for weather_output in weather_shown
+#   puts weather_output 
+# end
+
+
+
 # # current_temperature = weather_data["temperature"]
 # # puts current_temperature 
 
