76c13c48977bcd1626a6aaa33c60b3096440911d
kelsey Brown <kelsey.brown@kellogg.northwestern.edu>
2025-02-04 03:20:11 +0000

    lab 5 practice

--- labs/5-loops.rb
+++ labs/5-loops.rb
@@ -1,46 +1,52 @@
-# To run this code, be sure your current working directory
-# is the same as where this file is located and then run:
-# ruby 5-loops.rb
+# # To run this code, be sure your current working directory
+# # is the same as where this file is located and then run:
+# # ruby 5-loops.rb
 
-# EXERCISE
-# Build a deck of cards. Given the following arrays of card data,
-# use a loop to write out the cards to the screen.
-clubs = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
+# # EXERCISE
+# # Build a deck of cards. Given the following arrays of card data,
+# # use a loop to write out the cards to the screen.
+# clubs = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
 
-# club_cards = "#{clubs} of clubs"
-# puts club_cards
+# # club_cards = "#{clubs} of clubs"
+# # puts club_cards
 
-for clubs in clubs 
-    club_cards = "#{clubs} of clubs"
-    puts club_cards 
-end 
+# # for club in clubs 
+# #     club_cards = "#{club} of clubs"
+# #     puts club_cards 
+# # end 
 
-diamonds = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
+# diamonds = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
 
-# diamond_cards = "#{diamonds} of diamonds"
-# puts diamond_cards
+# # diamond_cards = "#{diamond} of diamonds"
+# # puts diamond_cards
 
-for diamonds in diamonds
-    diamond_cards = "#{diamonds} of diamonds"
-    puts diamond_cards 
-end 
-hearts = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
+# for diamond in diamonds
+#     diamond_cards = "#{diamond} of diamonds"
+#     puts diamond_cards 
+# end 
+# hearts = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
 
-for hearts in hearts 
-    heart_cards = "#{hearts} of hearts"
-    puts heart_cards
-end
-spades = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
+# for heart in hearts 
+#     heart_cards = "#{heart} of hearts"
+#     puts heart_cards
+# end
+cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
 
-for spades in spades 
-    spade_cards = "#{spades} of spades"
-    puts spade_cards
-end
+# for spade in spades 
+#     spade_cards = "#{spade} of spades"
+#     puts spade_cards
+# end
 
-puts "combined"
+# puts "combined"
 
-card_deck = [club_cards], [diamond_cards], [heart_cards], [spade_cards]
-puts card_deck 
+# for rank in cards
+#     puts "#{rank} of spades"
+#     puts "#{rank} of hearts"
+#     puts "#{rank} of diamonds"
+#     puts "#{rank} of clubs"
+# end
+
+# hand = puts ()
 
 # Sample output:
 # 2 of Clubs
@@ -55,9 +61,37 @@ puts card_deck
 # The arrays are identical for each suit and can be simplified by using
 # a ranks array as seen below. Try to complete the exercise again by
 # combining these arrays.
-# ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
-# suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
+ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
+suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
+
+#answer
+# for suit in suits
+#         for rank in ranks
+#             puts "#{rank} #{suit}"
+#         end
+# end
+
+
 
 # CHALLENGE #2
 # Deal a poker hand. Shuffle the deck and "deal" (i.e. display) a 5 card hand (i.e. 5 cards from the deck).
 # You will want to look at the documentation for Arrays: https://ruby-doc.org/core-2.7.0/Array.html
+
+deck =[]
+for suit in suits
+    for rank in ranks
+        cards = "#{rank} #{suit}"
+        deck.push(cards)
+    end
+end
+puts deck
+
+shuffeled_deck = deck.shuffle
+puts shuffeled_deck
+
+puts "dealer hand"
+hand = shuffeled_deck[0,5]
+
+for card in hand 
+    puts card
+end
