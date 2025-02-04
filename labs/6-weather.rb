e8c8f2344e46959966fa393559b995702e317284
kelsey Brown <kelsey.brown@kellogg.northwestern.edu>
2025-02-04 16:32:39 +0000

    lab 6 ruby intro

--- labs/6-weather.rb
+++ labs/6-weather.rb
@@ -15,20 +15,50 @@
 # - 60 degrees and Rainy
 # Note: only display the chance of rain if above 50%.
 # Note: do not display the chance of rain if conditions are already rainy.
+
 weather_data = {
   current: {
     temperature: 67,
     conditions: "Sunny"
   },
   forecast: [
-    { temperature: 65, conditions: "Mostly Cloudy", precipitation: 0.4 },
-    { temperature: 70, conditions: "Partly Cloudy", precipitation: 0.2 },
-    { temperature: 62, conditions: "Sunny", precipitation: 0.1 },
-    { temperature: 68, conditions: "Sunny", precipitation: 0.1 },
-    { temperature: 65, conditions: "Partly Cloudy", precipitation: 0.7 },
-    { temperature: 72, conditions: "Partly Cloudy", precipitation: 0.4 },
-    { temperature: 60, conditions: "Rainy", precipitation: 0.9 }
+    "Day 1" => { temperature: 65, conditions: "Mostly Cloudy", precipitation: 0.4 },
+    "Day 2" => { temperature: 70, conditions: "Partly Cloudy", precipitation: 0.2 },
+    "Day 3" => { temperature: 62, conditions: "Sunny", precipitation: 0.1 },
+    "Day 4" => { temperature: 68, conditions: "Sunny", precipitation: 0.1 },
+    "Day 5" => { temperature: 65, conditions: "Partly Cloudy", precipitation: 0.7 },
+    "Day 6" => { temperature: 72, conditions: "Partly Cloudy", precipitation: 0.4 },
+    "Day 7" => { temperature: 60, conditions: "Rainy", precipitation: 0.9 }
   ]
 }
-let current_weather = weather_data["current"]["temperature"]["conditions"]
-puts "#{current weather}"
+
+# Print current weather
+puts "Currently it is #{weather_data[:current][:temperature]} degrees and #{weather_data[:current][:conditions]}."
+
+# Print forecast
+puts "Forecast for the next 7 days:"
+weather_data[:forecast].each do |day, details|
+  temperature = details[:temperature]
+  conditions = details[:conditions]
+  precipitation = details[:precipitation]
+  
+  forecast_str = " - #{temperature} degrees and #{conditions}"
+
+  if conditions != "Rainy" && precipitation > 0.5
+    forecast_str += " with a #{(precipitation * 100).to_i}% chance of rain"
+  end
+
+  puts forecast_str
+end
+# # current_temperature = weather_data["temperature"]
+# # puts current_temperature 
+
+# for temperature in weather_data[forecast]
+#   puts temperature
+# end
+# # weather_summary =[]
+# for temperature in forecast 
+#   puts = "#{temperature}" "#{conditions}" "#{precipitation}"
+# end
+# # current_temperature = weather_data["current"]["temperature"]
+# # puts "#{current_temperature}"
